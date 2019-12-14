f54a3a1c152b46d99a601c75cab10db4f1ca26d7
Kevin VanHorn <kevin.vanhorn@utsouthwestern.edu>
2019-12-14 11:51:51 -0600

    Rain materials

---
+++ Source/KeepThePuppyDry/PUmbrella.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "PUmbrella.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KEEPTHEPUPPYDRY_API APUmbrella : public AStaticMeshActor
+{
+	GENERATED_BODY()
+
+public:
+	APUmbrella();
+	
+};
