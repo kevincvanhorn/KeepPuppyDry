aa708a03381831c4760293b07b974658d3fea309
Kevin VanHorn <kevin.vanhorn@utsouthwestern.edu>
2019-12-13 23:10:25 -0600

    backup

---
+++ Source/KeepThePuppyDry/PPuppyActor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/SkeletalMeshActor.h"
+#include "PPuppyActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KEEPTHEPUPPYDRY_API APPuppyActor : public ASkeletalMeshActor
+{
+	GENERATED_BODY()
+	
+};
